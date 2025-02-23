d5d00f576d981d1076972c973c8b67042dc9936a
riririririiiiii <145469004+riririririiiiii@users.noreply.github.com>
2025-02-23 19:36:37 +0500

    Delete Lab5/shop directory

--- Lab5/shop/src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { ProductListComponent } from './product-list/product-list.component';
-import { ProductItemComponent } from './product-item/product-item.component';
-import { CategoriesComponent } from './category/category.component';
-
-export const routes: Routes = [
-    { path: '', redirectTo: 'products', pathMatch: 'full' }, 
-    { path: 'products', component: ProductListComponent }    
-  ];
